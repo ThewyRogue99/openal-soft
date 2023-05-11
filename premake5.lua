0afa058fcac99bc6ceb56a0eedbb93e6ab38ea9c
ThewyRogue99 <batuhan.turk05@hotmail.com>
2023-05-11 14:25:51 +0300

    Removed unnecessary files for DragonEngine

---
+++ premake5.lua
@@ -0,0 +1,62 @@
+project "OpenAL-Soft"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++14"
+	staticruntime "off"
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	includedirs
+	{
+		"alc",
+		"common",
+		"include"
+	}
+
+	files
+	{
+		"**.h",
+		"**.cpp"
+	}
+
+	excludes
+	{
+		"alc/mixer/mixer_neon.cpp"
+	}
+
+	defines
+	{
+		"AL_LIBTYPE_STATIC"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"AL_BUILD_LIBRARY",
+			"AL_ALEXT_PROTOTYPES",
+			"_CRT_SECURE_NO_WARNINGS",
+			"NOMINMAX",
+			"OpenAL_EXPORTS"
+		}
+
+		links
+		{
+			"winmm"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
